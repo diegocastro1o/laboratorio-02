c0cdc82ed7bd5444655642490e3ee36473cc9023
juanmartinucu <juanmartinlopezmo@gmail.com>
2026-06-17 19:49:36 -0300

    Update usuarios.py
    cambio de excepciones en el registro de usuarios

--- src/logica/usuarios.py
+++ src/logica/usuarios.py
@@ -1,37 +1,70 @@
-from persistencia import USUARIOS, PRESTAMOS
-
-# TODO: Agregar los models (tipado de entidades) en los parámetros de la función.
-#       Importar de `utils.models` solo los necesarios (ej: `from utils.models import Usuarios,...`)
-#       y aplicarlos a cada parametro (ej: `usuarios: Usuarios`)
-
-print(USUARIOS)
-print(PRESTAMOS)
+from persistencia import cargar_datos
+from utils.models import Usuarios,Prestamos
+from utils.constants import DATA_FILE
 
 
-def registrar_usuario(usuarios, numero_socio, nombre):
-    if numero_socio in usuarios:
-        print("Error")
-            # excepcion
-    else:
+#ln (registro usuario):1) existe usuario ?
+#                             si: lanzas excepcion
+#                             no: crear diccionario del nuevo usuario
+#                      2)agregarlo a al diccionario "usuario"
+def registrar_usuario(usuarios:Usuarios, numero_socio:str, nombre:str):
+    try:
+        if numero_socio in usuarios:
+            raise Exception ("Usuario existente", usuarios[numero_socio])
         usuarios[numero_socio]={
-                                  "numero_socio": numero_socio,
-                                  "nombre": nombre,
-                                  "prestamos_activos": []
-                                }
+                            "numero_socio": numero_socio,
+                            "nombre": nombre,
+                            "prestamos_activos": []
+                            }       
+        return usuarios
+    except Exception as e:
+        print(e)
+    
 
-def dar_baja_usuario(usuarios, prestamos,numero_socio):
-    if numero_socio in usuarios:
-        if usuarios[numero_socio]["prestamos_activos"] == []:
-            del usuarios[numero_socio]
-        # else:
-            # excepcion
-    # else:
-        # excepcion
+#LN (dar de baja usuario) existe usuario ?
+        #                 no: lanzar excepcion
+        #                 si: tiene prestamos activos?
+        #                       si: lanzar excepcion
+        #                       no: borrar usuario del diccionario usuario
+class PrestamosException(Exception):
+    pass
 
-def historial_usuario(prestamos, numero_socio):
-    lista=[]
-    n=len(prestamos)
-    for x in range(n):
-        if prestamos[x]["numero_socio"]==numero_socio:
-            lista.append(prestamos[x])
-    return lista
+def dar_baja_usuario(usuarios:Usuarios, prestamos:Prestamos,numero_socio:str):
+    try:     
+        if numero_socio in usuarios:
+            if usuarios[numero_socio]["prestamos_activos"]==[]:
+                del usuarios[numero_socio]
+            else:
+                raise PrestamosException("El usuario tiene prestamos activo,no se puede dar de baja",ususario[numero_socio][prestamos_activos])
+        else:
+            raise KeyError("El usuario no existe")
+        
+        
+    except PrestamosException as e:
+        print(e)
+    except KeyError as b:
+        print(b)
+    
+
+#LN (historial prestamos) buscar en la lista de prestamos , si hay algo con numero_socio elegido
+                        # crear lista vacia y si los prestamos son del numero_socio, agregarlo a la lista vacia
+                        # la lista devolveria el numero_socio con el isbn y si fue devuelto o no
+                        #exceptions si : no existe socio
+def historial_usuario(prestamos:Prestamos, numero_socio:str):
+    try:
+        lista=[]
+        n=len(prestamos)
+        verificador=0
+        for x in range(n):
+            if prestamos[x]["numero_socio"]==numero_socio:
+                lista.append(prestamos[x]) 
+        if len(lista)==0:
+            raise KeyError("El usuario no existe")
+        return lista
+    
+    except KeyError as e:
+        print(e)
+
+data = cargar_datos(DATA_FILE)
+prestamos, usuarios, catalogo = data['prestamos'], data['usuarios'], data['catalogo']
+print(registrar_usuario(usuarios,'002', 'PPSP'))
